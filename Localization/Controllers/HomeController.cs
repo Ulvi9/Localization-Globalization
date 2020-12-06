24bf09612cc0d98503e69b8c1fc95a1911f29897
ulvi macid <ulvizm@code.edu.az>
2020-12-06 09:03:14 +0400

    First Commit

---
+++ Localization/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Localization.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc.Localization;
+
+namespace Localization.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IHtmlLocalizer<HomeController> _localizer;
+
+        public HomeController(ILogger<HomeController> logger,
+                              IHtmlLocalizer<HomeController> localizer)
+        {
+            _logger = logger;
+            _localizer = localizer;
+        }
+
+        public IActionResult Index()
+        {
+            var test = _localizer["HelloWorld"];
+            ViewData["HelloWorld"] = test;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CultureManagement(string culture,string returnUrl)
+        {
+            Response.Cookies.Append(CookieRequestCultureProvider.DefaultCookieName,
+                CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)),
+                new CookieOptions{Expires = DateTimeOffset.Now.AddDays(30)});
+            return LocalRedirect(returnUrl);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
+        }
+    }
+}
